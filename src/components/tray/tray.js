d395f34e267f7bc00cccba4163eb6b2e43746219
Matt Watson <mattwat@gmail.com>
2020-09-24 21:50:21 -0400

    create tray context

--- src/components/tray/tray.js
+++ src/components/tray/tray.js
@@ -1,20 +1,40 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { ChevronDownIcon } from '../icons'
 
+export const TrayContext = React.createContext({})
+
+export const useTray = () => {
+  return useContext(TrayContext)
+}
+
+export const TrayProvider = ({ children }) => {
+  const [active, setActive] = useState(false)
+  
+  const closeTray = () => setActive(false)
+  const toggleTray = () => setActive(!active)
+
+  return (
+    <TrayContext.Provider value={{ active, closeTray, toggleTray }}>
+      { children }
+    </TrayContext.Provider>
+  )
+}
+
 const Wrapper = styled.div`
-  background-color: #dde;
+  background-color: #334;
   position: fixed;
   bottom: 0;
   left: 0;
   width: 100%;
+  filter: drop-shadow(0 -0.25rem 0.25rem rgba(0, 0, 0, 0.2));
 `
 
 const Header = styled.div`
   font-weight: bold;
   cursor: pointer;
-  color: #333;
+  color: #eef;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -29,33 +49,39 @@ const OpenIndicator = styled.div(({ active }) => `
     justify-content: center;
     align-items: center;
     svg {
-        transition: fill 250ms, filter 250ms;
-        fill: ${ active ? 'var(--color-crimson)' : '#333' };
+        transition: fill 500ms, filter 250ms, transform 500ms;
+        transform: rotate(${ active ? '0' : '180deg' });
+        fill: ${ active ? 'var(--color-crimson)' : '#eef' };
     }
 `)
 
 const Content = styled.div(({ height }) => `
-  background-color: #ccd;
+  background-color: #111;
   transition: max-height 250ms;
   max-height: ${ height }px;
   overflow: hidden;
   padding: 0 1rem;
+  & a {
+    color: #eee;
+    text-decoration: underline;
+    &:hover {
+      color: var(--color-lightgrey);
+    }
+  }
 `)
 
 export const Tray = ({ title, children }) => {
-  const [active, setActive] = useState(false)
-
-  const handleToggle = event => setActive(!active)
+  const { active, toggleTray } = useContext(TrayContext)
 
   return (
     <Wrapper>
-      <Header onClick={ handleToggle }>
+      <Header onClick={ toggleTray }>
         <div className="title">{ title }</div>
         <OpenIndicator active={ active }>
             <ChevronDownIcon size={ 24 } active={ active } />
         </OpenIndicator>
       </Header>
-      <Content height={ active ? '200' : '0' }>
+      <Content height={ active ? '300' : '0' }>
         { children }
       </Content>
     </Wrapper>
