06bce79c6a1da95ec0968b956feb8373a6b493e7
Matt Watson <mattwat@gmail.com>
2020-09-25 01:27:23 -0400

    shrink tray width on large screens

--- src/components/tray/tray.js
+++ src/components/tray/tray.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { ChevronDownIcon } from '../icons'
+import { useWindowWidth } from '../../hooks'
 
 export const TrayContext = React.createContext({})
 
@@ -20,17 +21,21 @@ export const TrayProvider = ({ children }) => {
   )
 }
 
-const Wrapper = styled.div`
-  background-color: #334;
+const Wrapper = styled.div(({ width }) => `
   position: fixed;
   bottom: 0;
-  left: 0;
-  width: 100%;
+  min-width: ${ width };
+  left: 50%;
+  transform: translateX(-50%);
+  transition: min-width 250ms, transform 250ms;
   filter: drop-shadow(0 -0.25rem 0.25rem rgba(0, 0, 0, 0.2));
   z-index: 2;
-`
+  border-top-left-radius: ${ width === '100%' ? '0' : '4px' };
+  border-top-right-radius: ${ width === '100%' ? '0' : '4px' };
+`)
 
-const Header = styled.div`
+const Header = styled.div(({ color }) => `
+  background-color: ${ color };
   font-weight: bold;
   cursor: pointer;
   color: #eef;
@@ -41,7 +46,8 @@ const Header = styled.div`
   & .title {
     flex: 1;
   }
-`
+  transition: background-color 250ms;
+`)
 
 const OpenIndicator = styled.div(({ active }) => `
     display: flex;
@@ -72,10 +78,11 @@ const Content = styled.div(({ active }) => `
 
 export const Tray = ({ title, children }) => {
   const { active, toggleTray } = useContext(TrayContext)
+  const { isCompact } = useWindowWidth()
 
   return (
-    <Wrapper>
-      <Header onClick={ toggleTray }>
+    <Wrapper width={ isCompact ? '100%' : '600px' }>
+      <Header onClick={ toggleTray } color={ active ? 'var(--color-crimson-dark)' : '#334' }>
         <div className="title">{ title }</div>
         <OpenIndicator active={ active }>
             <ChevronDownIcon size={ 24 } fill="#eef" active={ active } />
